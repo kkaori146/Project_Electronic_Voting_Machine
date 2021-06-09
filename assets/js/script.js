ad947085edef0d180d9168ec5e7b6514eb5308f3
Katia Kaori Kaminishikawahara <kkaori146@gmail.com>
2021-06-09 15:34:24 +0900

    Funcionalidade dos botões CONFIRMA, CORRIGE e BRANCO

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,15 @@ let lateral = document.querySelector('.d-1-right');
 let numeros = document.querySelector('.d-1-3');
 
 let etapaAtual = 0;
-let numero= '';
+let numero = '';
+let votoBranco = false;
 
 function comecarEtapa() {
     let etapa = etapas[etapaAtual];
 
     let numeroHtml = '';
+    numero = '';
+    votoBranco = false;
 
     for(let i=0;i<etapa.numeros;i++) {
         if(i == 0 ) {
@@ -46,13 +49,18 @@ if(candidato.length > 0) {
     descricao.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
 let fotosHtml = '';
 for(let i in candidato.fotos) {
-    fotosHtml += `<div class="d-1-image"><img src="assets/images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
+    if(candidato.fotos[i].small) {
+        fotosHtml += `<div class="d-1-image small"><img src="assets/images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
+    } else {
+        fotosHtml += `<div class="d-1-image"><img src="assets/images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
+    }
+    
 }
 lateral.innerHTML = fotosHtml;
 } else {
     seuVotoPara.style.display = 'block';
     aviso.style.display = 'block';
-    descricao.innerHTML = `<div class="aviso--grande">VOTO NULO</div>`;
+    descricao.innerHTML = `<div class="aviso--grande pisca">VOTO NULO</div>`;
 }
 }
 
@@ -72,13 +80,37 @@ let elNumero = document.querySelector('.numero.pisca');
 
 }
 function branco() {
-    alert("Clicou em BRANCO!");
+   numero = '';
+VotoBranco = true;
+seuVotoPara.style.display = 'block';
+aviso.style.display = 'block';
+numeros.innerHTML = '';
+descricao.innerHTML =  '<div class = "aviso--grande pisca">VOTO EM BRANCO</div>';
+  lateral.innerHTML = '';  
 }
 function corrige() {
-    alert("Clicou em CORRIGE!");
+    comecarEtapa();
 }
 function confirma() {
-    alert("Clicou em CONFIRMA!");
+    let etapa = etapas[etapaAtual];
+
+let cotoConfirmado = false;
+
+    if(votoBranco === true) {
+        votoConfirmado = true;
+        console.log("Confirmando como BRANCO...");
+    } else if(numero.length === etapa.numeros) {
+votoConfirmado = true;
+        console.log("Confirmando como "+numero);
+}
+if(votoConfirmado) {
+    etapaAtual++;
+    if(etapas[etapaAtual] !== undefined) {
+        comecarEtapa();
+    } else {
+        console.log("FIM!");
+    }
+}
 }
 comecarEtapa();
 
